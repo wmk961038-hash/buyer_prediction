cab053910d81bff686ca0dc2ac2cbfef8f01511c
wmk961038-hash <wmk961038@gmail.com>
2026-03-22 20:35:33 +0800

    增加函数loss_function_partial的功能实现

--- PurchasePrediction.py
+++ PurchasePrediction.py
@@ -3,6 +3,6 @@ import numpy as np
 import pandas as pd
 #导入数据集
 df=pd.read_csv('ecommerce_data.csv')
-df['constant']=1
-num=df.iloc[0,1]
-print(num)
+df['constant']=1 #增加最后一项，用于处理偏置项
+
+print(df.head())
